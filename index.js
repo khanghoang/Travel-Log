3ed97c6b179624fbea38687288ebf76cb4bdf606
KhangHoang <hoangtrieukhang@gmail.com>
2015-09-04 00:31:58 +0700

    Add react router

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
 import 'babel-core/polyfill';
 import React from 'react';
 import { Provider } from 'react-redux';
-import App from './containers/App';
+import MainPage from './containers/App';
 import configureStore from './store/configureStore';
 
 const store = configureStore();
 
-React.render(
-  <Provider store={store}>
-    {() => <App />}
-  </Provider>,
-  document.getElementById('root')
+var Router = require('react-router');
+import {RouteHandler, Route}  from 'react-router';
+
+
+var App = React.createClass({
+  render () {
+    return (
+      <div>
+        <h1>App</h1>
+        <RouteHandler/>
+      </div>
+    )
+  }
+});
+
+var Login = React.createClass({
+  render () {
+    return (
+      <div>
+        <h1>Login Page</h1>
+      </div>
+    )
+  }
+});
+
+var MainPageHandler = React.createClass({
+  render () {
+    return (
+      <Provider store={store}>
+      {() => <MainPage />}
+      </Provider>
+    )
+  }
+});
+
+// declare our routes and their hierarchy
+var routes = (
+  <Route handler={App}>
+    <Route path="/login" handler={Login}/>
+    <Route path="/main_page" handler={MainPageHandler}/>
+  </Route>
 );
+
+Router.run(routes, Router.HashLocation, (Root) => {
+  React.render(<Root/>, document.body);
+});
