b27f728659c75191681c4b48b0b9f1edad526513
KhangHoang <hoangtrieukhang@gmail.com>
2015-09-05 23:39:58 +0700

    add function make random string

--- actions/index.js
+++ actions/index.js
@@ -45,6 +45,15 @@ function receiveLogin(data, err) {
   }
 }
 
+function _makeid()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+}
+
 function callLogin(username) {
   return dispatch => {
     dispatch(requestLogin(username));
