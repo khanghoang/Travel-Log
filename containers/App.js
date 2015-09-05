d70d79aa45fe2270f76ff3eb3ad00f7037c69cc3
KhangHoang <hoangtrieukhang@gmail.com>
2015-09-05 22:30:41 +0700

    remove unused code

--- containers/App.js
+++ containers/App.js
@@ -7,99 +7,41 @@ import Posts from '../components/Posts';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleRefreshClick = this.handleRefreshClick.bind(this);
   }
 
   componentDidMount() {
-    const { dispatch, selectedReddit } = this.props;
-    dispatch(fetchPostsIfNeeded(selectedReddit));
   }
 
   componentWillReceiveProps(nextProps) {
-    if (nextProps.selectedReddit !== this.props.selectedReddit) {
-      const { dispatch, selectedReddit } = nextProps;
-      dispatch(fetchPostsIfNeeded(selectedReddit));
-    }
   }
 
   handleChange(nextReddit) {
-    this.props.dispatch(selectReddit(nextReddit));
   }
 
   handleRefreshClick(e) {
-    e.preventDefault();
-
-    const { dispatch, selectedReddit } = this.props;
-    dispatch(invalidateReddit(selectedReddit));
-    dispatch(fetchPostsIfNeeded(selectedReddit));
   }
 
   render() {
-    const { currentUser, selectedReddit, posts, isFetching, lastUpdated } = this.props;
+    const { currentUser } = this.props;
     return (
       <div>
         <h2>Hi {currentUser.name}</h2>
-        <Picker value={selectedReddit}
-                onChange={this.handleChange}
-                options={['reactjs', 'frontend']} />
-        <p>
-          {lastUpdated &&
-            <span>
-              Last updated at {new Date(lastUpdated).toLocaleTimeString()}.
-              {' '}
-            </span>
-          }
-          {!isFetching &&
-            <a href="#"
-               onClick={this.handleRefreshClick}>
-              Refresh
-            </a>
-          }
-        </p>
-        {isFetching && posts.length === 0 &&
-          <h2>Loading...</h2>
-        }
-        {!isFetching && posts.length === 0 &&
-          <h2>Empty.</h2>
-        }
-        {posts.length > 0 &&
-          <div style={{ opacity: isFetching ? 0.5 : 1 }}>
-            <Posts posts={posts} />
-          </div>
-        }
       </div>
     );
   }
 }
 
 App.propTypes = {
-  selectedReddit: PropTypes.string.isRequired,
-  posts: PropTypes.array.isRequired,
   isFetching: PropTypes.bool.isRequired,
-  lastUpdated: PropTypes.number,
   dispatch: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state) {
-  const { selectedReddit, postsByReddit } = state;
   let { currentUser } = state;
   currentUser = currentUser || {};
-  const {
-    isFetching,
-    lastUpdated,
-    items: posts
-  } = postsByReddit[selectedReddit] || {
-    isFetching: true,
-    items: []
-  };
 
   return {
-    selectedReddit,
-    posts,
-    isFetching,
-    lastUpdated,
-    currentUser
+    currentUser,
   };
 }
 
